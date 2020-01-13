3bcda3b37fe558348d36a10ac93f21ca4d47ff00
Gagnon, Marc <marc.gagnon1@cgi.com>
2020-01-13 14:33:25 -0500

    time of visits endpoint

--- server/src/database/views.js
+++ server/src/database/views.js
@@ -2,7 +2,11 @@
 
 const Record = require('../schemas/Record');
 
-const { VIEWS_TYPE_UNIQUE, VIEWS_TYPE_TOTAL } = require('../constants/views');
+const {
+  VIEWS_TYPE_UNIQUE,
+  VIEWS_TYPE_TOTAL,
+  VIEWS_TYPE_TIME,
+} = require('../constants/views');
 
 const getUnique = async id => {
   return Record.aggregate([
@@ -78,12 +82,56 @@ const getTotal = async id => {
   ]);
 };
 
+const getTime = async id => {
+  return Record.aggregate([
+    {
+      $match: {
+        clientId: {
+          $exists: true,
+          $ne: null,
+        },
+        domainId: id,
+      },
+    },
+    {
+      $group: {
+        _id: {
+          day: {
+            $dayOfMonth: '$created',
+          },
+          month: {
+            $month: '$created',
+          },
+          year: {
+            $year: '$created',
+          },
+          hour: {
+            $hour: '$created',
+          },
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $sort: {
+        '_id.year': -1,
+        '_id.month': -1,
+        '_id.day': -1,
+      },
+    },
+  ]);
+};
+
 const get = async (id, type) => {
   switch (type) {
     case VIEWS_TYPE_UNIQUE:
       return getUnique(id);
     case VIEWS_TYPE_TOTAL:
       return getTotal(id);
+    case VIEWS_TYPE_TIME:
+      return getTime(id);
   }
 };
 
--- server/src/routes/views.js
+++ server/src/routes/views.js
@@ -1,46 +1,63 @@
-'use strict'
+'use strict';
 
-const { createError } = require('micro')
+const { createError } = require('micro');
 
-const views = require('../database/views')
+const views = require('../database/views');
 
 const {
-	VIEWS_TYPE_UNIQUE,
-	VIEWS_TYPE_TOTAL
-} = require('../constants/views')
+  VIEWS_TYPE_UNIQUE,
+  VIEWS_TYPE_TOTAL,
+  VIEWS_TYPE_TIME,
+} = require('../constants/views');
 
-const response = (entry) => ({
-	type: 'view',
-	data: {
-		id: {
-			day: entry._id.day,
-			month: entry._id.month,
-			year: entry._id.year
-		},
-		count: entry.count
-	}
-})
+const response = entry => ({
+  type: 'view',
+  data: {
+    id: {
+      day: entry._id.day,
+      month: entry._id.month,
+      year: entry._id.year,
+    },
+    count: entry.count,
+  },
+});
 
-const responses = (entries) => ({
-	type: 'views',
-	data: entries.map(response)
-})
+const responseTime = entry => ({
+  type: 'times',
+  data: {
+    id: {
+      day: entry._id.day,
+      month: entry._id.month,
+      year: entry._id.year,
+      hour: entry._id.hour,
+    },
+    count: entry.count,
+  },
+});
 
-const get = async (req) => {
+const responses = (entries, type) => ({
+  type: 'views',
+  data: entries.map(type !== VIEWS_TYPE_TIME ? response : responseTime),
+});
 
-	const { domainId } = req.params
-	const { type } = req.query
+const get = async req => {
+  const { domainId } = req.params;
+  const { type } = req.query;
 
-	const entries = await views.get(domainId, type)
+  const entries = await views.get(domainId, type);
 
-	switch (type) {
-		case VIEWS_TYPE_UNIQUE: return responses(entries)
-		case VIEWS_TYPE_TOTAL: return responses(entries)
-		default: throw createError(400, 'Unknown type')
-	}
-
-}
+  switch (type) {
+    case VIEWS_TYPE_UNIQUE:
+      return responses(entries, type);
+    case VIEWS_TYPE_TOTAL:
+      return responses(entries, type);
+    case VIEWS_TYPE_TIME:
+      return responses(entries, type);
+    default:
+      throw createError(400, 'Unknown type');
+  }
+};
 
 module.exports = {
-	get
-}
+  get,
+};
