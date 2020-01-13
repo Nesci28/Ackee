d52cb654756e92f5982f08a0ce85c35014e4a4bf
Gagnon, Marc <marc.gagnon1@cgi.com>
2020-01-13 14:44:56 -0500

    small build bugs fix

--- Ackee-client/src/app/components/shared/date-picker/date-picker.component.ts
+++ Ackee-client/src/app/components/shared/date-picker/date-picker.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { StateService } from "../../../services/state.service";
+import { takeUntil } from "rxjs/internal/operators/takeUntil";
+import { BaseComponent } from "../base/base.component";
 
 @Component({
   selector: "app-date-picker",
   templateUrl: "./date-picker.component.html",
   styleUrls: ["./date-picker.component.scss"]
 })
-export class DatePickerComponent implements OnInit {
+export class DatePickerComponent extends BaseComponent implements OnInit {
+  disabled: boolean;
+
   form: FormGroup = new FormGroup({
     singleDate: new FormControl("", Validators.required)
   });
 
-  constructor(private stateService: StateService) {}
+  constructor(private stateService: StateService) {
+    super();
+  }
 
   ngOnInit() {
     this.singleDate.setValue(this.stateService.singleDate$.value);
@@ -20,6 +26,12 @@ export class DatePickerComponent implements OnInit {
     this.form.valueChanges.subscribe(_ => {
       this.stateService.singleDate$.next(this.singleDate.value);
     });
+
+    this.stateService.datePickerDisable$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((disabled: boolean) => {
+        this.disabled = disabled;
+      });
   }
 
   get singleDate() {
