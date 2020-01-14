2b997c5d80eeb8710ecee5c71b5822c039c2936a
Gagnon <marc.gagnon1@cgi.com>
2020-01-14 16:43:34 -0500

    added ackee-tracker and update triggered events

---
+++ ackee-tracker/build.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { writeFile } = require('fs');
+const { promisify } = require('util');
+const js = require('rosid-handler-js');
+const save = promisify(writeFile);
+
+js('src/scripts/main.js', {
+  optimize: true,
+  browserify: {
+    standalone: 'ackee-tracker',
+  },
+}).then(data => {
+  return save('../server/src/ackee-tracker.min.js', data);
+});
